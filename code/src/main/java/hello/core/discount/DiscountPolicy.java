c4c35c99872b706728d52a49add73b50bba475d8
yeGenieee <cyj95428@gmail.com>
2021-04-30 23:21:45 +0900

    Add discountPolicy

--- code/src/main/java/hello/core/discount/DiscountPolicy.java
+++ code/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -1,2 +1,14 @@
-package hello.core.discount;public interface DiscountPolicy {
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인 금액
+     */
+    int discount(Member member, int price);
 }
