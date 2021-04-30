537cfba914896742352dc586473ff86e34a91aa9
yeGenieee <cyj95428@gmail.com>
2021-04-30 22:59:54 +0900

    Add MemberServiceTest

---
+++ code/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+}
