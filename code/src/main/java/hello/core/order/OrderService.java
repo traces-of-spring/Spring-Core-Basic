68892e7a62db60809f945b9de8f5fd0453d62211
yeGenieee <cyj95428@gmail.com>
2021-04-30 23:32:52 +0900

    Add Order & OrderService & OrderServiceImpl

--- code/src/main/java/hello/core/order/OrderService.java
+++ code/src/main/java/hello/core/order/OrderService.java
@@ -1,2 +1,5 @@
-package hello.core.order;public interface OrderService {
+package hello.core.order;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPrice);
 }
