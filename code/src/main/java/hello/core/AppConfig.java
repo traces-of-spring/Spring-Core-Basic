2e0165936ff20f9807e1f209e743a0fff3856e3c
yeGenieee <cyj95428@gmail.com>
2021-05-04 17:04:13 +0900

    Modify Discount Policy

--- code/src/main/java/hello/core/AppConfig.java
+++ code/src/main/java/hello/core/AppConfig.java
@@ -25,6 +25,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
