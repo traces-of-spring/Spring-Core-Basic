7cac470634a97e85ea3c694a3721706d73612d9c
yeGenieee <cyj95428@gmail.com>
2022-11-21 20:57:03 +0900

    feat: 새로운 할인 정책의 적용

--- code/src/main/java/hello/core/ch03/AppConfig.java
+++ code/src/main/java/hello/core/ch03/AppConfig.java
@@ -24,6 +24,7 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
