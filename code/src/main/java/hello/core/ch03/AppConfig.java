9ab9f556f23aaa677ad494208d4557ccc770495d
yeGenieee <cyj95428@gmail.com>
2022-11-21 20:50:56 +0900

    feat: AppConfig 리팩터링 (중복 제거 및 역할에 따른 구현이 보이게)

--- code/src/main/java/hello/core/ch03/AppConfig.java
+++ code/src/main/java/hello/core/ch03/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core.ch03;
 
+import hello.core.ch03.discount.DiscountPolicy;
 import hello.core.ch03.discount.FixDiscountPolicy;
 import hello.core.ch03.discount.RateDiscountPolicy;
+import hello.core.ch03.member.MemberRepository;
 import hello.core.ch03.member.MemberService;
 import hello.core.ch03.member.MemberServiceImpl;
 import hello.core.ch03.member.MemoryMemberRepository;
@@ -11,10 +13,17 @@ import hello.core.ch03.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
