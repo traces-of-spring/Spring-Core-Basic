f4984a9e296fb697f2e79d301d0efa2226c8f560
yeGenieee <cyj95428@gmail.com>
2022-11-22 16:43:09 +0900

    feat: BeanDefinition(스프링 빈 설정 메타 정보)

---
+++ code/src/test/java/hello/core/ch04/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.ch04;
+
+import hello.core.ch03.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfigTest.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
