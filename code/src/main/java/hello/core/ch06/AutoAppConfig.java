df868e9412f82f9aa2378b9e8cc42be481cd6fe5
yeGenieee <cyj95428@gmail.com>
2022-11-22 23:28:06 +0900

    중복 등록과 충돌

--- code/src/main/java/hello/core/ch06/AutoAppConfig.java
+++ code/src/main/java/hello/core/ch06/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core.ch06;
 
+import hello.core.ch06.member.MemberRepository;
+import hello.core.ch06.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,5 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
 }
