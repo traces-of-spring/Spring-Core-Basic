a300a4536ac75f2d8cbee520efb090364abd0650
yeGenieee <cyj95428@gmail.com>
2022-11-26 20:49:16 +0900

    feat: 조회한 빈이 모두 필요한 경우

--- code/src/test/java/hello/core/ch07/allbean/AllBeanTest.java
+++ code/src/test/java/hello/core/ch07/allbean/AllBeanTest.java
@@ -2,6 +2,9 @@ package hello.core.ch07.allbean;
 
 import hello.core.ch07.AutoAppConfig;
 import hello.core.ch07.discount.DiscountPolicy;
+import hello.core.ch07.member.Grade;
+import hello.core.ch07.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +12,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class AllBeanTest {
     @Test
     void findAllBean() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "user!", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPrice).isEqualTo(2000);
     }
 
     static class DiscountService {
@@ -25,6 +40,11 @@ public class AllBeanTest {
             this.policies = policies;
             System.out.println("policyMap = " + policyMap);
             System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
         }
     }
 }
